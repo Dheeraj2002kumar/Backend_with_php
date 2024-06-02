c38a4e24ced5cd4c031f7bf2639059ed0b374aa6
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-06-02 18:05:11 +0530

    Update validate_form_data_with_php.php

--- Backend_php/PHP Form Validation/PHP Form Validation/validate_form_data_with_php.php
+++ Backend_php/PHP Form Validation/PHP Form Validation/validate_form_data_with_php.php
@@ -57,3 +57,40 @@ echo $gender;
 
 </body>
 </html>
+
+
+
+/*
+
+How To Avoid $_SERVER["PHP_SELF"] Exploits?
+$_SERVER["PHP_SELF"] exploits can be avoided by using the htmlspecialchars() function.
+
+The form code should look like this:
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+The htmlspecialchars() function converts special characters to HTML entities. Now if the user tries to exploit the PHP_SELF variable, it will result in the following output:
+
+<form method="post" action="test_form.php/&quot;&gt;&lt;script&gt;alert('hacked')&lt;/script&gt;">
+The exploit attempt fails, and no harm is done!
+
+Validate Form Data With PHP
+The first thing we will do is to pass all variables through PHP's htmlspecialchars() function.
+
+When we use the htmlspecialchars() function; then if a user tries to submit the following in a text field:
+
+<script>location.href('http://www.hacked.com')</script>
+- this would not be executed, because it would be saved as HTML escaped code, like this:
+
+&lt;script&gt;location.href('http://www.hacked.com')&lt;/script&gt;
+The code is now safe to be displayed on a page or inside an e-mail.
+
+We will also do two more things when the user submits the form:
+
+Strip unnecessary characters (extra space, tab, newline) from the user input data (with the PHP trim() function)
+Remove backslashes \ from the user input data (with the PHP stripslashes() function)
+The next step is to create a function that will do all the checking for us (which is much more convenient than writing the same code over and over again).
+
+We will name the function test_input().
+
+Now, we can check each $_POST variable with the test_input() function, and the script looks like this:
+*/
