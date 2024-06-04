61ebf5c4d077b358dc7ce58eacb80b3b94700965
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-06-04 18:57:38 +0530

    Add files via upload

---
+++ Backend_php/PHP Advance/PHP Date and Time/get_date.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Get Date</title>
+</head>
+<body>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-M-D") . "<br>";
+echo "Today is " . date("l");
+?>
+  
+</body>
+</html>
