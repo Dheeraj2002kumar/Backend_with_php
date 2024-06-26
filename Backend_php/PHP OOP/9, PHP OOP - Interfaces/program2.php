880a2a81b6ad7e3096657d7d849d8b1245bb67e7
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-06-26 11:00:42 +0530

    Add files via upload

---
+++ Backend_php/PHP OOP/9, PHP OOP - Interfaces/program2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+
+<?php
+// Interface definition
+interface Animal {
+  public function makeSound();
+}
+
+// Class definitions
+class Cat implements Animal {
+  public function makeSound() {
+    echo "Meow!";
+  }
+}
+
+class Dog implements Animal {
+  public function makeSound() {
+    echo " Woof! ";
+  }
+}
+
+class Mouse implements Animal {
+  public function makeSound() {
+    echo " Squeak ";
+  }
+}
+
+
+// Create a list of animals
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+// Tell the animals to make a sound
+foreach($animals as $animal){
+  $animal->makeSound();
+}
+?>
+  
+</body>
+</html>
